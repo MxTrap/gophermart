4891d3dcae53f83e56ba4775152165a29ed39f9d
mxtrap <makstrap.23.08@gmail.com>
2025-04-14 15:13:16 +0300

    fix login

--- internal/gophermart/entity/user.go
+++ internal/gophermart/entity/user.go
@@ -3,7 +3,9 @@ package entity
 type Token string
 
 type User struct {
-	ID       int64
-	Login    string
-	Password string
+	ID        int64
+	Login     string
+	Password  string
+	Balance   float32
+	Withdrawn float32
 }
