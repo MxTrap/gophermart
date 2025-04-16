ee84216f25a06b29b7bf2f9801e1f0194aa4d980
mxtrap <makstrap.23.08@gmail.com>
2025-04-16 19:59:58 +0300

    try to implement fanIn fanOut

--- internal/gophermart/services/order_worker.go
+++ internal/gophermart/services/order_worker.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"context"
+	"errors"
+	"sync"
 	"time"
 
 	"github.com/MxTrap/gophermart/internal/gophermart/entity"
@@ -14,7 +16,7 @@ type accrualService interface {
 
 type storage interface {
 	Push(el entity.Order)
-	Get() *entity.Order
+	Get(elemCount int) []entity.Order
 }
 
 type orderBalanceRepo interface {
@@ -42,58 +44,141 @@ func NewOrderWorkerService(
 	}
 }
 
+type result struct {
+	order entity.Order
+	err   error
+}
+
 func (*OrderWorkerService) isTerminalStatus(status string) bool {
 	return status == entity.OrderInvalid || status == entity.OrderProcessed
 }
 
-func (s *OrderWorkerService) worker(ctx context.Context, id int, job chan entity.Order) {
-	log := s.log.With("worker#", id)
-	const maxRetryCount = 3
-	const retryPause = 5
-outer:
-	for order := range job {
-		var accrualOrder entity.Order
-		var err error
-		for i := range maxRetryCount {
-			accrualOrder, err = s.svc.GetOrderAccrual(order.Number)
-			if err == nil && accrualOrder.Status != order.Status {
-				accrualOrder.UserID = order.UserID
-				accrualOrder.Number = order.Number
-				err := s.repo.UpdateOrderBalance(ctx, accrualOrder)
-				if err != nil {
-					log.Error(err)
+func (s *OrderWorkerService) save(ctx context.Context, ch chan result) {
+	go func() {
+		for res := range ch {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if res.err != nil {
+					s.log.Error("failed to save ch: %v", res.err)
 				}
-				if s.isTerminalStatus(accrualOrder.Status) {
-					break outer
+				err := s.repo.UpdateOrderBalance(ctx, res.order)
+				if err != nil || !s.isTerminalStatus(res.order.Status) {
+					s.storage.Push(res.order)
 				}
 			}
-			time.Sleep(time.Duration(i*retryPause) * time.Second)
 		}
-		if err != nil {
-			log.Error(err)
+	}()
+}
+
+func (s *OrderWorkerService) update(ctx context.Context, inputChan chan entity.Order) chan result {
+	resultCh := make(chan result)
+
+	go func() {
+		defer close(resultCh)
+		for order := range inputChan {
+			accrualOrder, err := s.svc.GetOrderAccrual(order.Number)
+			if accrualOrder.Status == order.Status {
+				err = errors.New("order has already been processed")
+			}
+			if err == nil {
+				accrualOrder.UserID = order.UserID
+				accrualOrder.Number = order.Number
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			case resultCh <- result{accrualOrder, err}:
+			}
 		}
-		s.storage.Push(order)
+
+	}()
+
+	return resultCh
+}
+
+func (*OrderWorkerService) generate(ctx context.Context, input []entity.Order) chan entity.Order {
+	inputCh := make(chan entity.Order)
+
+	go func() {
+		defer close(inputCh)
+
+		for _, data := range input {
+			select {
+			case <-ctx.Done():
+				return
+			case inputCh <- data:
+			}
+		}
+	}()
+
+	return inputCh
+}
+
+func (s *OrderWorkerService) fanOut(ctx context.Context, input chan entity.Order) []chan result {
+	numWorkers := 5
+
+	channels := make([]chan result, numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		resultCh := s.update(ctx, input)
+		channels[i] = resultCh
 	}
+
+	return channels
+}
+
+func (*OrderWorkerService) fanIn(ctx context.Context, results []chan result) chan result {
+	finalCh := make(chan result)
+
+	var wg sync.WaitGroup
+
+	for _, ch := range results {
+		chClosure := ch
+
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			for data := range chClosure {
+				select {
+				case <-ctx.Done():
+					return
+				case finalCh <- data:
+				}
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(finalCh)
+	}()
+
+	return finalCh
+
 }
 
 func (s *OrderWorkerService) Run(ctx context.Context) {
 	const jobNum = 5
-	jobs := make(chan entity.Order, jobNum)
+	const updateDelay = time.Second * 5
+
 	go func(ctx context.Context) {
 		for {
 			select {
 			case <-ctx.Done():
-				close(jobs)
 				return
 			default:
-				order := s.storage.Get()
-				if order != nil {
-					jobs <- *order
-				}
+				orders := s.storage.Get(jobNum)
+				inputCh := s.generate(ctx, orders)
+				channels := s.fanOut(ctx, inputCh)
+				resultCh := s.fanIn(ctx, channels)
+				s.save(ctx, resultCh)
 			}
+
+			time.Sleep(updateDelay)
 		}
 	}(ctx)
-	for i := range jobNum {
-		go s.worker(ctx, i, jobs)
-	}
 }
--- internal/gophermart/services/storage.go
+++ internal/gophermart/services/storage.go
@@ -18,12 +18,18 @@ func (s *Storage) Push(el entity.Order) {
 	*s.queue = append(*s.queue, el)
 }
 
-func (s *Storage) Get() *entity.Order {
+func (s *Storage) Get(elemCount int) []entity.Order {
 	self := *s.queue
-	var el *entity.Order
-	if len(self) > 0 {
-		el, *s.queue = &self[0], self[1:]
+	var el []entity.Order
+	if len(self) == 0 {
+		return el
 	}
+	if len(self) > elemCount {
+		el, *s.queue = self[:elemCount+1], self[elemCount+1:]
+		return el
+	}
+
+	el, *s.queue = *s.queue, []entity.Order{}
 
 	return el
 }
